d43a956aebe261fa66b90892dbdd64a422893368
Your Name <you@example.com>
2022-12-09 15:44:57 +0700

    lap6_1 completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 int main(){
+
+    int num;
+    int Odd = 0;
+    int Even = 0;
+    
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+            
+            while ( num != 0 )
+        { 
+                if( num % 2 == 0 ){
+
+                Even += 1;
+
+                }else{
+                    Odd += 1;
+                }
+                cout << "Enter an integer: ";
+                cin >> num;
+        }
+    
+    cout << "#Even numbers = " << Even << "\n";
+    cout << "#Odd numbers = " << Odd << "\n";
     return 0;
 }
